795b184db861a792f84dcb1c78f5189e722b99c4
andressa fazam <andressafazam@gmail.com>
2020-03-08 02:15:36 -0300

    Add as funções campanhaVacina, contarvacinados e vacinado e atualização das funções listarPets, adicionarPet e buscarPet (arquivos petshop.js e index.js)

--- index.js
+++ index.js
@@ -9,45 +9,91 @@ const server = http
     let urlCompleta = url.parse(req.url, true);
     let queryString = urlCompleta.query; // parametros
     let rota = urlCompleta.pathname; // ex: pets/add
-
-    // console.log(queryString);
+    let conteudo = "";
+    let listarPets = `
+    Lista de pets: 
+    -------------------------`;
 
     switch (rota) {
+      case "/":
+        res.write(`
+    -------------------------
+      Bem vindo ao Petshop!
+    -------------------------`);
+        break;
+
       case "/pets":
-        let conteudo = petshop.listarPets();
+        conteudo = petshop.listarPets();
         if (conteudo.length > 0) {
-          res.write(conteudo);
+          res.write(listarPets + conteudo);
         } else {
-          res.write("Nenhum pet cadastrado :(");
+          res.write("Nenhum pet cadastrado.");
         }
         break;
+
       case "/pets/add":
         let novoPet = queryString;
         if (petshop.adicionarPet(novoPet)) {
-          res.write(`${novoPet.nome} foi adicionado a nossa lista!`);
+          res.write(`
+        ${novoPet.nome} foi adicionado a nossa lista!`);
         } else {
           res.write("Ops, algo deu errado!");
         }
         break;
+
       case "/pets/buscar":
         let nomePet = queryString.nome;
         let petsEncontrados = petshop.buscarPet(nomePet);
         if (petsEncontrados.length > 0) {
           res.write(
-            `Encontramos ${petsEncontrados.length} pets com o nome ${nomePet}`
-          );
+            `
+        Encontramos ${petsEncontrados.length} pets com o nome ${nomePet}`);
         } else {
-          res.write("Ops, nenhum pet cadastrado com esse nome!");
+          res.write(`
+        Nenhum pet cadastrado com esse nome!`);
         }
         break;
+
+      case "/pets/vacina":  
+        let nomePetVacina = queryString.nome;
+        if(petshop.vacinado(nomePetVacina)){
+          res.write(
+            `
+        O pet ${nomePetVacina} foi vacinado com sucesso.`);
+        } else{
+          res.write(
+            `
+        Algum problema ocorreu e o pet ${nomePetVacina} não foi vacinado com sucesso.`);
+        }
+        break;
+
+      case "/pets/vacinados":  
+        let qttVacinados = petshop.contarVacinados();
+        res.write(
+            `
+        De todos os pets cadastrados ${qttVacinados} já foram vacinados.`);
+        break;
+
+      case "/pets/campanha":
+        if (petshop.campanhaVacina()){
+          res.write(
+            `
+        Campanha de vacinação foi um sucesso, todos os pets foram vacinados!`);
+        } else{
+          res.write(
+            `
+        Campanha de vacinação não foi um sucesso...`);
+        }
+        break;
+
       default:
-        res.write("tô perdido");
-    }
+              res.write(`
+        Pagina não encontrada...`);
+          }
 
     // req = request, res = responses
     res.end();
   })
   .listen(3000, "localhost", () => {
-    // quando ligo servidor
-    console.log("Servidor rodando :)");
+    console.log("Servidor rodando!");
   });
--- petshop.js
+++ petshop.js
@@ -1,25 +1,64 @@
-let pets = [{ nome: "doug" }, { nome: "costelinha" }];
+let pets = [
+  { nome: "Snoopy", tipo: "Cachorro", raca: "Beagle", idade: 70, genero: "M", vacinado: true, servicos: [] }, 
+  { nome: "Mingau", tipo: "Gato", raca: "Angora", idade: 31, genero: "M", vacinado: false, servicos: [] },
+  { nome: "Bidu", tipo: "Cachorro", raca: "Schnauzer", idade: 61, genero: "M", vacinado: false, servicos: [] },
+  { nome: "Garfield", tipo: "Gato", raca: "Persa", idade: 42, genero: "M", vacinado: true, servicos: [] },
+  { nome: "Jujuba", tipo: "Cachorro", raca: "ViraLata", idade: 10, genero: "F", vacinado: false, servicos: [] },
+  { nome: "Titi", tipo: "Cachorro", raca: "ViraLata", idade: 6, genero: "F", vacinado: false, servicos: [] },
+];
 
 const listarPets = () => {
+  let cont = 1;
   let conteudo = "";
   for (let pet of pets) {
-    conteudo += `
-    -----------
-    Nome: ${pet.nome}
-    -----------`;
+    conteudo += ` 
+    ${cont++}. ${pet.nome}
+    --------------------------`;
   }
-
   return conteudo;
 };
 
 const adicionarPet = novoPet => {
-  return pets.push(novoPet);
+  pets.push(novoPet);
+  return listarPets();
 };
 
 const buscarPet = nomePet => {
-  let petsEncontrados = pets.filter(pet => pet.nome == nomePet);
-
-  return petsEncontrados;
+  let numIndex = [];
+  pets.filter((pets, index) => {
+    if(pets.nome == nomePet)
+      numIndex.push(index);
+  });
+  return numIndex;
 };
 
-module.exports = { listarPets, adicionarPet, buscarPet };
+const vacinado = novoPet =>{
+  let validacao = false;
+  let numIndex = buscarPet(novoPet);
+  if(numIndex.length > 0){
+    pets[numIndex].vacinado = true;
+    validacao = true;
+  }
+  return validacao;
+}
+
+const contarVacinados = () => {
+  contador = 0;
+  pets.filter(pets => {
+    if(pets.vacinado == true)
+      contador++;
+  });
+  return contador
+}
+
+const campanhaVacina = () => {
+  pets.forEach(pets => {
+    pets.vacinado = true;
+  });
+  if(contarVacinados() == pets.length)
+    return true;
+  else
+    return false;
+}
+
+module.exports = { listarPets, adicionarPet, buscarPet, vacinado, contarVacinados, campanhaVacina};
